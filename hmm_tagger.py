3aac03e9295cb4f92d52a7560fe0877db1dfc10b
selvaram ganesh <gsriram7@gmail.com>
2018-02-15 07:10:48 -0800

    Extracting bigrams and tag freq for computing Transition probability

--- hmm_tagger.py
+++ hmm_tagger.py
@@ -2,26 +2,58 @@ import utils as utils
 
 file = open("en_train_tagged.txt", "r")
 
-word_to_tag = {}
+word_to_tags = {}
 tag_to_words = {}
+word_freq = {}
+tag_freq = {}
+word_and_tag_freq = {}
+bigrams = {}
+
+
+def get_bigram_freq_dict(first_tag, second_tag, bigrams_dict):
+    temp = bigrams_dict.get(first_tag, {})
+    bigram_freq = temp.get(second_tag, 0)
+    temp[second_tag] = bigram_freq + 1
+    return temp
+
 
 for line in file.readlines():
     line = line.strip("\n")
+    first_tag = "start_213"
+    end_tag = "end_146"
     for word in line.split(" "):
         (w, tag) = utils.split_word_from_tag(word)
+        second_tag = tag
 
-        t_for_a_w = word_to_tag.get(w, set())
+        bigrams[first_tag] = get_bigram_freq_dict(first_tag, second_tag, bigrams)
+
+        w_and_tag = w + "/" + tag
+
+        word_freq[w] = word_freq.get(w, 0) + 1
+        tag_freq[tag] = tag_freq.get(tag, 0) + 1
+        word_and_tag_freq[w_and_tag] = word_and_tag_freq.get(w_and_tag, 0) + 1
+
+        t_for_a_w = word_to_tags.get(w, set())
         t_for_a_w.add(tag)
-        word_to_tag[w] = t_for_a_w
+        word_to_tags[w] = t_for_a_w
 
         w_for_a_t = tag_to_words.get(tag, set())
         w_for_a_t.add(w)
         tag_to_words[tag] = w_for_a_t
 
+        first_tag = second_tag
+
+    bigrams[first_tag] = get_bigram_freq_dict(first_tag, end_tag, bigrams)
+
 file.close()
 
-print word_to_tag['In']
+print word_to_tags['In']
 print tag_to_words['IN']
 print "Total tags %s" % len(tag_to_words)
-print "Total unique words %s" % len(word_to_tag)
-print word_to_tag.keys()
+print "Total unique words %s" % len(word_to_tags)
+print word_to_tags.keys()
+print tag_freq
+print word_freq
+print word_and_tag_freq
+print sum(bigrams['start_213'].values())
+print bigrams
